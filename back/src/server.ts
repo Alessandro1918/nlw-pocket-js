91c149eb63b8f0ebda937ef7336cd956e2ac3686
Alessandro1918 <alessandro1918@hotmail.com>
2024-09-16 17:09:17 -0300

    feat: add '@fastify/cors'

--- back/src/server.ts
+++ back/src/server.ts
@@ -4,6 +4,7 @@ import {
   validatorCompiler,
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
+import fastifyCors from "@fastify/cors"
 import { createGoalRoute } from "./routes/create-goal"
 import { getWeekPendingGoalsRoute } from "./routes/get-week-pending-goals"
 import { createGoalCompletionRoute } from "./routes/create-goal-completion"
@@ -18,6 +19,10 @@ const PORT = 4000
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifyCors, {
+  origin: "*"
+})
 
 app.get("/", () => {return "Hello, world!"})
 
