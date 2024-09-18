959a6bae35e229ab2765138d44c2a16c3968de54
Alessandro1918 <alessandro1918@hotmail.com>
2024-09-18 18:13:15 -0300

    feat: new component: create-goal

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,21 +1,13 @@
-import { Plus } from "lucide-react"
-import logo from "./assets/in-orbit-logo.svg"
-import letsStart from "./assets/rocket-launch-illustration.svg"
-import { Button } from "./component/ui/button"
+import { Dialog } from "./component/ui/dialog"
+import { CreateGoal } from "./component/create-goal"
+import { EmptyGoals } from "./component/empty-goals"
 
 export function App() {
 
   return (
-    <div className="h-screen flex flex-col items-center justify-center gap-8">
-      <img src={logo} alt="in.orbit"/>
-      <img src={letsStart} alt="lets start"/>
-      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
-        Você ainda não cadastrou nenhuma meta, que tal cadastrar uma agora mesmo?
-      </p>
-      <Button>
-        < Plus className="size-4"/>
-        Cadastrar metae
-      </Button>
-    </div>
+    <Dialog>
+      <EmptyGoals />
+      <CreateGoal />
+    </Dialog>
   )
 }
