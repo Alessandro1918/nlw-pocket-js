3e29be1d130403e26bbd192c5864e98a6599d592
Alessandro1918 <alessandro1918@hotmail.com>
2024-09-18 12:14:47 -0300

    style: added new UI components

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,7 @@
 import { Plus } from "lucide-react"
 import logo from "./assets/in-orbit-logo.svg"
 import letsStart from "./assets/rocket-launch-illustration.svg"
+import { Button } from "./component/ui/button"
 
 export function App() {
 
@@ -11,10 +12,10 @@ export function App() {
       <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
         Você ainda não cadastrou nenhuma meta, que tal cadastrar uma agora mesmo?
       </p>
-      <button className="px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 flex items-center gap-2 text-sm font-medium tracking-tight hover:bg-violet-600">
+      <Button>
         < Plus className="size-4"/>
-        Cadastrar meta
-      </button>
+        Cadastrar metae
+      </Button>
     </div>
   )
 }
