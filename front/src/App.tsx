97f305d2a2cc13a270f0949fee3bcd3987442502
Alessandro1918 <alessandro1918@hotmail.com>
2024-09-19 16:08:28 -0300

    feat: new page: summary

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,14 @@
 import { Dialog } from "./component/ui/dialog"
 import { CreateGoal } from "./component/create-goal"
 import { EmptyGoals } from "./component/empty-goals"
+import { Summary } from "./component/summary"
 
 export function App() {
 
   return (
     <Dialog>
-      <EmptyGoals />
+      {/* <EmptyGoals /> */}
+      <Summary />
       <CreateGoal />
     </Dialog>
   )
